5c2aab2e4cfe421dcca0187b8069a4516f6c19ec
Taeke de Haan <Taeke.dehaan@live.nl>
2018-10-02 15:29:05 +0200

    small adjustments exercise 5

--- Optimization_A1.m
+++ Optimization_A1.m
@@ -12,19 +12,20 @@ Db1 = 5;
 Db2 = 6;
 Db3 = 8;
 
-E1 = Da1 + Db1;
+E_1 = Da1 + Db1;
 E_2 = Da2 + Db2;
-E3 = Da3 + Db3;
+E_3 = Da3 + Db3;
 
 % Maximum monthly Battery cell production
-B = (5+E1)*10^6;
+B = (5+E_1)*10^6;
 B_R = 4E3;
 B_W = 6E3;
 
 % Employees at Edison
 E = 100 + E_2; 
 E_h = 160; % max working hours per employee
-C_E = 3000 + 50*E3; % Monthly salary (?) independent of hours
+C_E = 3000 + 50*E_3; % Monthly salary (?) independent of hours
+E_A = 0:72; % Additional workers
 
 % Required time to produce model R and W (hours)
 T = E*E_h; % ?????????????
@@ -33,7 +34,7 @@ T_W = 15;
 
 
 % Storage space, produced cars ned to be stored for 1 month!
-S = (15+E3)*10^3; %available
+S = (15+E_3)*10^3; %available
 S_R = 10;         % Required for 1 Model R
 S_W = 12;         % Required for 1 Model W
 
@@ -45,60 +46,74 @@ C_W = 45000;
 P_R = 55000;
 P_W = 75000;
 
-%% question 1
+%% question 2
 c = -[(P_R-C_R), (P_W-C_W), 0, 0, 0]';
-Aeq = [B_R B_W 1 0 0;
+A = [B_R B_W 1 0 0;
      T_R T_W 0 1 0;
      S_R S_W 0 0 1];
-beq = [B T S]';
+b = [B T S]';
 lb = [0 0 0 0 0]';
 ub = [+Inf +Inf +Inf +Inf +Inf]';
 
-x = linprog(c,[], [], Aeq,beq,lb,ub);
+x = linprog(c,[], [], A,b,lb,ub);
+disp('====QUESTION 2====')
 disp(x)
-
-%% Question 2
+disp(['profit: ', num2str(-c' * x - C_E * E)]);
+%% Question 3
 Rmax = 1000; % maximum of R sold
 
 c = -[(P_R-C_R), (P_W-C_W), 0, 0, 0, 0]';
-Aeq = [ B_R B_W 1 0 0 0;
+A = [ B_R B_W 1 0 0 0;
         T_R T_W 0 1 0 0;
         S_R S_W 0 0 1 0;
         1   0   0 0 0 1];
-beq = [B T S Rmax]';
+b = [B T S Rmax]';
 lb = [0 0 0 0 0 0]';
 ub = [+Inf +Inf +Inf +Inf +Inf +Inf]';
 
-x = linprog(c,[], [], Aeq,beq,lb,ub);
-disp(x);
+x = linprog(c,[], [], A,b,lb,ub);
 
+disp('====QUESTION 3====')
+disp(x)
+disp(['profit: ', num2str(-c' * x - C_E * E)]);
 %% Question 4
 % update constants
-B = (8 + E1)*10^6; % [batterycells/month] produced
-S = (22+E3)*10^3; %[m^2] storage space
+clear x
 
-for i = 0:72
-    E = 100 + E_2 + i;
-    T_R = 10 - 1/12 * i;
-    T_W = 15 - 1/12 * i;
-    T = E * 160;
+B = (8 + E_1)*10^6; % [batterycells/month] produced
+S = (22 + E_3)*10^3; %[m^2] storage space
+
+for i = E_A
+    E = 100 + E_2 + i; % total amount of employees
+    T_R = 10 - 1/12 * i; % new manufactoring time R
+    T_W = 15 - 1/12 * i; % new manufactoring time W
+    T = E * 160; % available working hours
     
     c = -[(P_R-C_R), (P_W-C_W), 0, 0, 0, 0]';
-    Aeq = [ B_R B_W 1 0 0 0;
+    A = [B_R B_W 1 0 0 0;
         T_R T_W 0 1 0 0;
         S_R S_W 0 0 1 0;
         1   0   0 0 0 1];
-    beq = [B T S Rmax]';
+    b = [B T S Rmax]';
     lb = [0 0 0 0 0 0]';
     ub = [+Inf +Inf +Inf +Inf +Inf +Inf]';
 
-    x(:,i+1) = linprog(c,[], [], Aeq,beq,lb,ub);
+    x(:,i+1) = linprog(c,[], [], A,b,lb,ub);
     
     profit(i+1) = -c' * x(:,end) - C_E * E;
     
 end
 
-figure
-plot(profit)
+%% results
+[maxProfit, iProfit] = max(profit);
+disp(['Maximum profit: ', num2str(maxProfit)]);
+disp(['Number of aditional workers: ', num2str(iProfit)])
+disp(x(:,iProfit));
 
-
+% plot
+figure('position',[500,100,1000,500])
+plot(E_A, profit)
+xlim([min(E_A), max(E_A)]);
+xlabel('Additional employees [-]')
+ylabel('Profit [euro]')
+saveas(gcf, 'result', 'epsc')

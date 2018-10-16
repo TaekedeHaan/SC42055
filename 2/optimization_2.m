1b833a7ef7650eed7ee9e99ef52a67c7e93ce147
Taeke de Haan <Taeke.dehaan@live.nl>
2018-10-16 13:42:29 +0200

    Finalizing asign. 2

--- 2/optimization_2.m
+++ 2/optimization_2.m
@@ -30,13 +30,14 @@ B = [-dt * a2, dt * a2];
 c = a1 * dt * T_amb;
 
 % objective function
-y = (T_1 - (A*T_0 + B*[Q_out Q_in].' + c))^2; % y(T_1, T_0, a1, dt, Q_in, a2, Q_out, T_amb)
+y = (T_1 - (A*T_0 + B*[Q_out Q_in].' + c))^2;
 y = expand(y);
 
-k_end = 100 + E_1;
+k_end = 100 + E_1; % itterations used
 k = 1:k_end;
 dt = 3600; % [s]
-%% Rewrite inhto normal form
+
+% Rewrite inhto normal form
 a = [a1, a2];
 
 % compute H
@@ -46,7 +47,6 @@ H = simplify(jacobian(dydx, a));
 % compute f
 F = simplify(dydx - a * H);
 
-%% optimize
 % read data
 temp = readtable('measurements.csv');
 
@@ -65,8 +65,8 @@ H_num = double(subs(H));
 F_num = sum(F_num,3);
 H_num = sum(H_num,3);
 
+% solve
 a = quadprog(H_num,F_num);
-% constants
 
 disp(['a1: ', num2str(a(1))]);
 disp(['a2: ', num2str(a(2))]);
@@ -77,6 +77,7 @@ clear Q_in
 N = 360;
 k = 1:N;
 
+% counderies
 Q_max = (100 + E_2)*10^3; %[W]
 Q_min = 0;
 T_min = 315; % [K]
@@ -84,13 +85,13 @@ T_min = 315; % [K]
 T_init = 330 + E_3; % [K]
 T_amb = repmat(275 + E_1, N, 1); %[K]
 
+% system constants
 a1 = 1.96 * 10^-7;
 a2 = 3.80 * 10^-9;
 
 % load data 
 temp = readtable('heatDemand.csv');
 Q_out = temp.Heat_demand(k);
-
 temp = readtable('inputPrices.csv');
 lambda = temp.Price(k); % [eur/MWh]
 lambda = lambda /(10^6 * 3600); % [eur/J]
@@ -102,7 +103,9 @@ c = a1 * dt * T_amb;
 
 % generate materices fcor optimization
 f = [dt * lambda(1:N); zeros(N + 1,1)];
-Aeq = [-diag(repmat(B(2),N,1)), -diag(repmat(A,N,1)) + diag(ones(N- 1,1),1), [zeros(N-1,1);1]]; % moving of temp
+Aeq1 = -diag(repmat(B(2),N,1));
+Aeq2 = [-diag(repmat(A,N,1)) + diag(ones(N- 1,1),1), [zeros(N-1,1);1]];
+Aeq = [Aeq1, Aeq2]; % moving of temp
  
 beq = B(1) * Q_out + c;
 lb = [zeros(N,1); T_init; repmat(T_min,N,1)];
@@ -113,8 +116,9 @@ x = linprog(f,[], [], Aeq,beq,lb,ub);
 Q_in = x(1:360);
 T = x(361:721);
 
-price1 = Q_in' * dt * lambda;
-disp(['price: ', num2str(price1), '[euro]']);
+% compute price
+cost_tot = Q_in' * dt * lambda; % [euro]
+disp(['price: ', num2str(cost_tot), '[euro]']);
 
 % plot
 figure('Position', [100,0,600,1100])
@@ -136,30 +140,35 @@ title('Temperature')
 xlabel('time [h]')
 ylabel('Temperature [K]')
 
-saveas(gcf, 'result', 'epsc')
+saveas(gcf, 'result3', 'epsc')
 
 %% Question 4
 disp('====QUESTION 4====')
 cost_add = (0.1+ E_2/10); %[euros/K^2]
-T_ref = 323;
-T_max = 368;
+T_ref = 323; %[K]
+T_max = 368; %[K]
+
+% compute quedratic cost terms
 H = zeros(2*N+1, 2*N+1);
 H(end, end) = 2 * cost_add;
 
+% c umpute linear costterms
 f = [dt * lambda(1:N); zeros(N,1); -2*T_ref * cost_add];
 
+% bounds
 lb= [repmat(Q_min, N, 1); T_init; repmat(T_min, N, 1)];
 ub= [repmat(Q_max, N, 1); T_init; repmat(T_max, N, 1)];
 
+% solve
 x = quadprog(H,f,[],[],Aeq,beq,lb,ub);
 Q_in = x(1:360);
 T = x(361:721);
 
-price1 = Q_in' * dt * lambda + cost_add * (T(end) - T_ref)^2;
-price2 = Q_in' * dt * lambda + cost_add*T(end)^2 + cost_add*T_ref^2 - cost_add*2*T(end)*T_ref;
+cost_tot = Q_in' * dt * lambda + cost_add * (T(end) - T_ref)^2; %[euro]
+cost_ter = cost_add * (T(end) - T_ref)^2; %[euro]
+disp(['total cost: ', num2str(cost_tot), '[euro]']);
+disp(['terminal cost: ', num2str(cost_ter), '[euro]']);
 
-disp(['price1: ', num2str(price1), '[euro]']);
-disp(['price2: ', num2str(price2), '[euro]']);
 % plot
 figure('Position', [100,0,600,1100])
 subplot(3,1,1)
